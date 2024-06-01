5d03ab05bbc190dc511f768d6d58d92bef4cc063
bilalakyuzz <bilalakyuzz@gmail.com>
2024-06-01 12:00:17 +0300

    fixes

---
+++ productservice/src/main/java/com/group3/productservice/ProductServiceApplication.java
@@ -0,0 +1,14 @@
+package com.group3.productservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class ProductServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductServiceApplication.class, args);
+    }
+
+}
