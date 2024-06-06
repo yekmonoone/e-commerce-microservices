a10d244d9b45b7a581e8977d2045c0c46697e32c
winused <nlytsl13@gmail.com>
2024-06-06 18:05:29 +0300

    Ek değişiklik yok

---
+++ reviewservice/src/main/java/com/example/reviewservice/dto/ReviewDTO.java
@@ -0,0 +1,11 @@
+package com.example.reviewservice.dto;
+
+@Data
+public class ReviewDTO {
+    private Long reviewId;
+    private String userName;
+    private int rating;
+    private String comment;
+
+    // Getters and setters
+}
