5602263feb1365990c8ab6266c2270cb1991a35c
Betül Şentürk <betul_senturk2000@hotmail.com>
2024-06-06 16:48:28 +0300

    paymentService commit

---
+++ paymentService/paymentService/src/main/java/com/example/paymentService/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package com.example.paymentService.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Payment Service API")
+                        .version("1.0")
+                        .description("API for processing and verifying payments"));
+    }
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("payments")
+                .pathsToMatch("/payment/**")
+                .build();
+    }
+}
