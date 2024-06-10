eed2442acaf1b6f2212c780a58383e83a515daf1
[Ozan Kaygalak] <[ozankaygalak@gmail.com]>
2024-06-10 16:02:40 +0300

    order front son

--- frontend/src/api/orderService.js
+++ frontend/src/api/orderService.js
@@ -1,19 +1,19 @@
-import axios from 'axios';
+  import axios from 'axios';
 
-const API_URL = 'http://localhost:8081/api/orders';
+  const API_URL = 'http://localhost:8081/api/orders';
 
-export const placeOrder = (order) => {
-    return axios.post(`${API_URL}/placeOrder`, order);
-};
-//a
-export const getOrderById = (orderId) => {
-    return axios.get(`${API_URL}/${orderId}`);
-};
+  export const placeOrder = (order) => {
+      return axios.post(`${API_URL}/placeOrder`, order);
+  };
+  //a
+  export const getOrderById = (orderId) => {
+      return axios.get(`${API_URL}/${orderId}`);
+  };
 
-export const findOrdersByUserId = (userId) => {
-    return axios.get(`${API_URL}/getOrdersByUserId/${userId}`);
-};
+  export const findOrdersByUserId = (userId) => {
+      return axios.get(`${API_URL}/getOrdersByUserId/${userId}`);
+  };
 
-export const getStatus = (orderId) => {
-    return axios.get(`${API_URL}/status/${orderId}`);
-};
+  export const getStatus = (orderId) => {
+      return axios.get(`${API_URL}/status/${orderId}`);
+  };
